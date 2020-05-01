1a5fe6e504d868d641f85670c3f90d2f617a3d90
Josh Polansky <joshp@autresgrp.com>
2020-05-01 12:58:45 -0700

    test Module

---
+++ drop.js
@@ -0,0 +1,90 @@
+// If the user clicks on a dropdow menu item find
+//  the nearest selected item in the dropdown and set the text
+// Expected structure:
+{
+    /* 
+    <el class ='dropup || dropdown'>
+        <el class='selected-item'>text</el>
+        <el class='dropdown-menu'>
+            <el class='dropdown-item'>
+                Text 1
+            </el>
+            <el class='dropdown-item'>
+                Text 2
+            </el>
+            <el class='dropdown-item'>
+                Text 3
+            </el>
+        </el>
+    </el> 
+    */
+}
+$(document).on({
+    'click': function (e) {
+        $(this).closest('.dropup, .dropdown').find('.selected-item').text($(this).text());
+    }
+}, '.dropdown-menu .dropdown-item')
+
+// Handle dropdown subs so that the dropdown doesn't close
+// Expected structure:
+{
+    /* 
+    <el class="dropup || dropdown">
+        <el data-toggle="dropdownsub"></el>
+        <el class="dropdown-menu">
+            <el class="dropdown-item"></el>
+            <el class="dropdown-item dropup || dropdown">
+                <el data-toggle="dropdownsub"></el>
+                <el class="dropdown-menu">
+                    <el class="dropdown-item"></el>
+                    <el class="dropdown-item"></el>
+                </el>
+            </el>
+        </el>
+    </el> 
+    */
+}
+$(document).on({
+    'click': function (e) {
+        var container = $(this);
+        if (container.is(e.target)) {
+            p = $(e.target).closest(".dropup, .dropdown")
+            p.toggleClass('open');
+            e.stopPropagation();
+            e.preventDefault();
+        }
+    }
+}, "[data-toggle='dropdownsub']");
+
+// Close dropdowns if we click off them or their subs
+// Expected structure:
+{0
+    /* 
+    <el class="dropup || dropdown">
+        <el data-toggle="dropdownsub"></el>
+        <el class="dropdown-menu">
+            <el class="dropdown-item"></el>
+            <el class="dropdown-item dropup || dropdown">
+                <el data-toggle="dropdownsub"></el>
+                <el class="dropdown-menu">
+                    <el class="dropdown-item"></el>
+                    <el class="dropdown-item"></el>
+                </el>
+            </el>
+        </el>
+    </el> 
+    */
+}
+$(document).on({
+    'mousedown': function (e) {
+        // Find all dropdowns
+        var container = $(".dropup, .dropdown");
+        // If the target of the click isn't in this dropdown, close this dropdown
+        container.each(element => {
+            $el = $(container[element])
+            if (!$el.is(e.target) && $el.has(e.target).length === 0) {
+                $el.removeClass('open');
+            }
+        });
+    }
+});
